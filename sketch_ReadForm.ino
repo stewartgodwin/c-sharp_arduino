cf50923948e0ba4be12acaa9561ab15bb34898a0
Dr Stewart Godwin <smg@iinet.net.au>
2019-03-11 10:56:11 +0800

    Add files via upload

---
+++ sketch_ReadForm.ino
@@ -0,0 +1,26 @@
+int redLED = 12; // Red LED
+int blueLED = 13; // Blue LED
+int buzzerPin  = 5; // Buzzer
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(redLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+}
+void loop() {
+  // LED and buzzer
+  int val = Serial.parseInt();
+  switch (val) {
+    case 100: digitalWrite(redLED, HIGH); break;
+    case 101: digitalWrite(redLED, LOW); break;
+    case 200: digitalWrite(blueLED, HIGH); break;
+    case 201: digitalWrite(blueLED, LOW); break;
+    case 300: {
+        tone(buzzerPin, 1000, 50);
+        delay(500);
+        tone(buzzerPin, 3000, 100); break;
+      }
+  } // end of switch
+}
+
